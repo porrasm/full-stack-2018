f2b077da53c25e166c55f2eeb7d8f6e5e7a9cc10
porrasm <31691452+porrasm@users.noreply.github.com>
2018-10-16 15:38:53 +0300

    Tehtävät 1.1-1.5

---
+++ Tehtävät/osa1/osa1app/src/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+          {
+            nimi: 'Reactin perusteet',
+            tehtavia: 10
+          },
+          {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7
+          },
+          {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14
+          }
+        ]
+      }
+    
+    
+
+    return (
+        <div>
+            <Otsikko kurssi={kurssi.nimi} />
+            <Sisalto osat={kurssi.osat} />
+            <Yhteensa osat={kurssi.osat} />
+        </div>
+    )
+}
+
+const Otsikko = (props) => {
+    return (
+        <h1>{props.kurssi}</h1>
+    )
+}
+
+const Sisalto = (props) => {
+    return (
+        <div>
+            <Osa osa={props.osat[0]} />
+            <Osa osa={props.osat[1]} />
+            <Osa osa={props.osat[2]} />
+        </div>
+    )
+}
+const Osa = (props) => {
+    return (
+        <p>{props.osa.nimi} {props.osa.tehtavia}</p>
+    )
+}
+
+const Yhteensa = (props) => {
+
+    const total = props.osat[0].tehtavia + props.osat[1].tehtavia + props.osat[2].tehtavia
+
+    return (
+        <p>
+            yhteensä {total} tehtävää
+        </p>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
